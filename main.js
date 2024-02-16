76cacc9a329ff5c00439f4811ff8242517135f3c
Emmanuel <emmanuel.lucio.urbina@outlookk.com>
2024-02-16 17:11:02 -0600

    init

---
+++ main.js
@@ -0,0 +1,26 @@
+// script.js file
+
+function domReady(fn) {
+	if (
+		document.readyState === "complete" ||
+		document.readyState === "interactive"
+	) {
+		setTimeout(fn, 1000);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
+domReady(function () {
+
+	// If found you qr code
+	function onScanSuccess(decodeText, decodeResult) {
+    document.getElementById("text").value = decodeText;
+	}
+
+	let htmlscanner = new Html5QrcodeScanner(
+		"my-qr-reader",
+		{ fps: 10, qrbos: 250 }
+	);
+	htmlscanner.render(onScanSuccess);
+});
